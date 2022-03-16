dbd0fd0727b5298e4cc8197795fad07348b16b0b
Andrew Eames <ajerxs@gmail.com>
2022-03-15 22:07:40 -0400

    commit tree deca3b1d23e7be43f24be12413ddfba3de86e9d7

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,18 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => 
+        <div key={actor.name}>
+            <h3>{actor.name}</h3>
+            <h4>Movies:</h4>
+            <ol>
+            {actor.movies.map(movie => 
+              <li>{movie}</li>
+              )}
+            </ol>
+        </div>
+          )}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,18 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => 
+        <div key={director.name}>
+          <h3>{director.name}</h3>
+            <h4>Movies:</h4>
+            <ol>
+            {director.movies.map(movie => 
+              <li>{movie}</li>
+              )}
+            </ol>
+        </div>
+        )}
     </div>
   );
 }
